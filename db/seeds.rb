e4a0677b4ca5b73a59011fd36785bd217b38daab
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-11-24 11:58:31 -0500

    seed data comes from fixtures, not spotify API
    Spotify API data will change, tests in ember app that connects to this API need to know how many songs, artist, albums.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
-spotify = RestClient.get("https://spotifycharts.com/api/?type=regional&country=global&recurrence=daily&date=latest&limit=200&offset=0")
-parsed = JSON.parse(spotify)
+require 'fileutils'
+# spotify = RestClient.get("https://spotifycharts.com/api/?type=regional&country=global&recurrence=daily&date=latest&limit=200&offset=0")
+# parsed = JSON.parse(spotify)
+
+# File.open('db/fixtures/seed_data.yml', 'w') { |file| file.write(spotify) }
+
+
+data = File.read('db/fixtures/seed_data.yml')
+parsed = JSON.parse(data)
 parsed["entries"]["items"].each do |item|
   album_name = item["track"]["album"]["name"]
   album = Album.where(name: album_name).first_or_create do |a|
