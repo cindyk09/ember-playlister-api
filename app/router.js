4719328d7d27a7cd06c7a84f7cd1482df36dc11b
prakashwaghwani <pwaghwani@gmail.com>
2016-02-16 16:52:17 -0500

    playlister finished

--- app/router.js
+++ app/router.js
@@ -6,8 +6,21 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  
+  this.route('artists', function() {
+    this.route('artist', {
+      path: ':artist_id'
+    });
+  });
+  this.route('albums', function() {
+    this.route('album', {
+      path: ':album_id'
+    });
+  });
+  this.route('songs', function() {
+    this.route('song', {
+      path: ':song_id'
+    });
+  });
 });
 
 export default Router;
- 
